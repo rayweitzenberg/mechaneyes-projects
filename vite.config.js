68eb2d064bc6242b20511e9d2a3db1ccd65333c0
Ray Weitzenberg <ray@neoneon.one>
2022-04-02 18:25:28 -0400

    vite: organizing pages into own subdirectories

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,14 @@ module.exports = defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        betterbreaks: resolve(__dirname, 'better-breaks/index.html'),
+        loops: resolve(__dirname, 'loops/index.html'),
+        monolyth: resolve(__dirname, 'monolyth/index.html'),
+        munariveil: resolve(__dirname, 'munari-veil/index.html'),
+        objects: resolve(__dirname, 'objects/index.html'),
+        soundnoir: resolve(__dirname, 'sound-noir/index.html'),
+        stereoh: resolve(__dirname, 'stereoh/index.html'),
+        whirligrid: resolve(__dirname, 'whirligrid/index.html'),
       }
     }
   }
